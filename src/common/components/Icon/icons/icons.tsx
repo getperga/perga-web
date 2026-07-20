a1e82bf79c78ffa668e12f090c51e38a92450d29
Dmitry Tiunov <dbtiunov@gmail.com>
2026-07-20 19:49:23 +0200

    Add search and close icons

--- src/common/components/Icon/icons/icons.tsx
+++ src/common/components/Icon/icons/icons.tsx
@@ -350,4 +350,32 @@ export const icons = {
       <line x1="12" y1="3" x2="12" y2="15" />
     </svg>
   ),
+  search: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <circle cx="11" cy="11" r="7" />
+      <line x1="21" y1="21" x2="16.65" y2="16.65" />
+    </svg>
+  ),
+  close: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <line x1="18" y1="6" x2="6" y2="18" />
+      <line x1="6" y1="6" x2="18" y2="18" />
+    </svg>
+  ),
 };
